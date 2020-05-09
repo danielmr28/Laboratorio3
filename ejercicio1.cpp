f2af6c869f26bfcbdb41be72252c5ccd4783f8a3
danielmr28 <00085920@uca.edu.sv>
2020-05-08 18:07:44 -0600

    prueba 2

---
+++ ejercicio1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1,num2;
+
+    cin >> num1 >> num2;
+
+    int suma=num1+num2;
+    
+    string nombre= "Daniel";
+
+    cout << nombre + ", El resultado de la suma es " << suma;
+
+    return 0;
+}
